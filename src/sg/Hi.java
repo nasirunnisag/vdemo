0116e714a1b64ba3085aa7eac165426d12f16930
nasir <nasir@DESKTOP-UKG2FJD>
2025-04-04 11:39:06 +0530

    hi

--- src/sg/Hi.java
+++ src/sg/Hi.java
@@ -6,7 +6,7 @@ public class Hi {
 public static void main(String[] args) {
 	System.out.println("hi");
 	System.out.println("hi2");
-	System.out.println("hi3");
+	System.out.println("hi3first1");
 	System.out.println("hi3");
 }
 }
